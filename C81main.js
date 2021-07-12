7ef791c74dd581c6b3dddb1cc62b4fb92fcabdc7
Whi833 <85868214+Whi833@users.noreply.github.com>
2021-07-12 20:17:08 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,25 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color="yellow";
+width = 3;
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = width;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown (e) {
+    mousex = e.clientX - canvas.offsetLeft;
+    mousey = e.clientY - canvas.offsetTop;
+    circle (mousex , mousey);
+
+} 
+function circle (mousex , mousey) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.arc(mousex,mousey,40,0,2*Math.PI);
+    ctx.stroke();
+
+} 
